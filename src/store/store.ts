aede91cbc96bbb7a6bac5c48ed160dc08e430e5f
gabeFrancisco <gabegit2@gmail.com>
2023-06-19 21:19:10 -0300

    instalado redux e configurado store

---
+++ src/store/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+export const store = configureStore({
+  reducer: {},
+  devTools: true
+})
+
+export const useAppDispatch: () => typeof store.dispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<ReturnType<typeof store.getState>> = useSelector;
